7d22539eccc12cfbce1235be4cc06293eeb766ea
BenMacD <benmdonagh@gmail.com>
2020-02-19 16:29:54 +0000

    Initial Commmit

---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = props => {
+    return (
+      <header id="welcome-section">
+        <div className="moon" />
+        <div className="silhouette" />
+      </header>
+    );
+  };
+
+export default Header;
